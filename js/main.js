a5a106901dec61b6cd88b81318a5db890cdd5017
SANYAM AGRAWAL <sanyam4u@gmail.com>
2015-03-26 01:44:55 +0530

    Adding First Google Map

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*! @license
- *  Project: Buttons
+ *  Project: Google Map
  *  Description: Google Map Interface which extends to multi market
  *  Author: Sanyam Agrawal
  *  Date : 26/03/2015 (DD/MM/YYYY)
@@ -8,7 +8,18 @@
 
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
-;(function ( $, window, document, undefined ) {
+;
+(function($, window, document, undefined) {
     "use strict";
+    var element = $("#map_canvas").get(0);
 
-})( jQuery, window, document);
+    function initialize() {
+        var mapOptions = {
+                zoom: 8,
+                center: new google.maps.LatLng(-34.397, 150.644)
+            },
+            map = new google.maps.Map(element, mapOptions);
+    }
+
+    google.maps.event.addDomListener(window, "load", initialize);
+})(jQuery, window, document);
