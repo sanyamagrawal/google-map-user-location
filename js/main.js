5fc58979172b014a7469598ebc6eb9ce2d1fb2f4
SANYAM AGRAWAL <sanyam4u@gmail.com>
2015-03-26 01:28:03 +0530

    Project Infrastructure

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*! @license
+ *  Project: Buttons
+ *  Description: Google Map Interface which extends to multi market
+ *  Author: Sanyam Agrawal
+ *  Date : 26/03/2015 (DD/MM/YYYY)
+ *  License: Apache License v2.0
+ */
+
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ( $, window, document, undefined ) {
+    "use strict";
+
+})( jQuery, window, document);
